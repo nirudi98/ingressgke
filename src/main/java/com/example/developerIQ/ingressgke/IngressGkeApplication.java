fbd0128dc351def177d23aa32589d38c963af69f
nirudi98 <niruddya.samarasinghe@gmail.com>
2023-12-09 22:15:46 +0530

    Initial commit

---
+++ src/main/java/com/example/developerIQ/ingressgke/IngressGkeApplication.java
@@ -0,0 +1,13 @@
+package com.example.developerIQ.ingressgke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressGkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngressGkeApplication.class, args);
+    }
+
+}
